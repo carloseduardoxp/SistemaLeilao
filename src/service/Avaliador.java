10da40803c4022d11cc54b483c1028f35b789e08
CarlosEduardo <CarlosEduardo@CarlosEdNote>
2015-05-22 11:40:22 -0300

    Inclusão do requisito "retornar 3 maiores lances"

--- src/service/Avaliador.java
+++ src/service/Avaliador.java
@@ -1,10 +1,17 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Avaliador {
 	
 	private Double maiorDeTodos = Double.NEGATIVE_INFINITY;
 	
 	private Double menorDeTodos = Double.POSITIVE_INFINITY;
+	
+	private List<Lance> maioresLances;
 	
 	public void avalia(Leilao leilao) {
 		for (Lance lance: leilao.getLances()) {
@@ -14,15 +21,31 @@ public class Avaliador {
 			if (lance.getValor() < menorDeTodos) {
 				menorDeTodos = lance.getValor();
 			}
-		}
+		}		
+		pegaOsMaioresNo(leilao);
 	}
 	
+	private void pegaOsMaioresNo(Leilao leilao) {
+		maioresLances = new ArrayList<Lance>(leilao.getLances());
+		Collections.sort(maioresLances, new Comparator<Lance>() {			
+			@Override
+			public int compare(Lance o1, Lance o2) {
+				return o2.getValor().compareTo(o1.getValor());
+			}
+		});
+		maioresLances = maioresLances.subList(0, maioresLances.size() >= 3? 3: maioresLances.size());
+	}
+
 	public Double getMenorLance() {
 		return menorDeTodos;
 	}
 	
 	public Double getMaiorLance() {
 		return maiorDeTodos;
+	}
+
+	public List<Lance> getMaioresLances() {
+		return maioresLances;
 	}
 
 }
--- test/service/AvaliadorTest.java
+++ test/service/AvaliadorTest.java
@@ -1,6 +1,10 @@
 package service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class AvaliadorTest {
@@ -95,6 +99,32 @@ public class AvaliadorTest {
 		assertEquals(maiorEsperado,leiloeiro.getMaiorLance(),0.0001);
 		
 		assertEquals(menorEsperado,leiloeiro.getMenorLance(),0.0001);						
+	}
+	
+	@Test
+	public void deveRetornar3MaioresLances() {
+		Usuario joao = new Usuario("Joao");
+		Usuario jose = new Usuario("Jose");
+		Usuario maria = new Usuario("Maria");
+
+		Leilao leilao = new Leilao("Playstation 3 novo");
+		
+		leilao.propoe(new Lance(jose,400.0));
+		leilao.propoe(new Lance(maria,250.0));
+		leilao.propoe(new Lance(joao,300.0));	
+		leilao.propoe(new Lance(joao,450.0));
+		leilao.propoe(new Lance(jose,600.0));
+		
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		List<Lance> maioresLances = leiloeiro.getMaioresLances();
+		
+		assertEquals(3,maioresLances.size());
+		assertEquals(600,maioresLances.get(0).getValor(),0.0001);
+		assertEquals(450,maioresLances.get(1).getValor(),0.0001);
+		assertEquals(400,maioresLances.get(2).getValor(),0.0001);
+								
 	}
 
 
